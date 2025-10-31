1fabd6bac196b4604bfdb68bb79f98258ac3c845
MaxFls <demianflores430@gmail.com>
2025-10-31 00:13:20 -0600

    Se agregan las rutas en el archivo settings

--- aplicaciones/trabajadores/apps.py
+++ aplicaciones/trabajadores/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TrabajadoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trabajadores'
+    name = 'aplicaciones.trabajadores'
