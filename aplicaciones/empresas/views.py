6cd678d7b7c57ef82b62388ebf302e148fdb1ef6
MaxFls <demianflores430@gmail.com>
2025-11-17 22:46:18 -0600

    Se agrega carpeta templates y se integran algunas plantillas

--- aplicaciones/empresas/views.py
+++ aplicaciones/empresas/views.py
@@ -3,22 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Enterprises Home Page")
+    return render(request, 'home.html')
 
 def SignUp(request):
-    return HttpResponse("REgistra tu empresa")
+    return render(request, 'signup.html')
 
 def SignIn(request):
-    return HttpResponse("Inicia sesión en tu cuenta")
+    return render(request, 'signin.html')
 
 def agregar_vacante(request):
-    return HttpResponse("Agregar una nueva vacante")
+    return render(request, 'agregar-vacante.html')
 
 def detalle_vacante(request, id):
-    return HttpResponse(f"Detalle de la vacante con ID: {id}")
+    return render(request, 'detalle-vacante.html', {'vacante_id': id})
 
 def editar_vacante(request, vacante_id):
-    return HttpResponse(f"Editar la vacante con ID: {vacante_id}")
+   return render(request, 'editar-vacante.html', {'vacante_id': vacante_id})
 
 def historial_vacantes(request):
-    return HttpResponse("Historial de vacantes publicadas")
+    return render(request, 'historial.html')
