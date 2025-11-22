501e45e124af65e2ea6e5850a4744e7901d4ee49
MaxFls <demianflores430@gmail.com>
2025-11-22 02:45:57 -0600

    Se agrega el modelo (tabla) de Empresa

--- aplicaciones/empresas/admin.py
+++ aplicaciones/empresas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Empresa#, Vacante
 
 # Register your models here.
+admin.site.register(Empresa)
--- aplicaciones/empresas/models.py
+++ aplicaciones/empresas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, null=False   , blank=False)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField(null=False, blank=False, unique=True, default='email')
+    biografia = models.TextField(null=False, blank=False, default='Biografía de la empresa.')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    categoria = models.CharField(max_length=300, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.nombre
