ec282d2707d9fbcd05a064c06e70ee80669d2429
MaxFls <demianflores430@gmail.com>
2025-11-25 04:55:32 -0600

    Pagina Básica de inicio agregada

--- aplicaciones/general/views.py
+++ aplicaciones/general/views.py
@@ -2,23 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the General Home Page")
 
-def SignUp(request):
-    return HttpResponse("REgistra tu empresa")
-
-def SignIn(request):
-    return HttpResponse("Inicia sesión en tu cuenta")
-
-def agregar_vacante(request):
-    return HttpResponse("Agregar una nueva vacante")
-
-def detalle_vacante(request, vacante_id):
-    return HttpResponse(f"Detalle de la vacante con ID: {vacante_id}")
-
-def editar_vacante(request, vacante_id):
-    return HttpResponse(f"Editar la vacante con ID: {vacante_id}")
-
-def historial_vacantes(request):
-    return HttpResponse("Historial de vacantes publicadas")
+def pagina_inicio(request):
+    return render(request, 'pagina_inicio.html')
